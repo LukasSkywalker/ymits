3757969713a73951aee4167786161ef3fba6a2a0
Lukas Diener <lukas.diener@hotmail.com>
2011-11-29 23:53:05 +0100

    Better memory management, no OOM Exceptions anymore

--- wp7apps/BingSlideshow/BingSlideshow/MainPage.xaml.cs
+++ wp7apps/BingSlideshow/BingSlideshow/MainPage.xaml.cs
@@ -15,6 +15,7 @@ using Microsoft.Phone.Controls;
 using System.Windows.Threading;
 using System.IO;
 using System.Net.NetworkInformation;
+using Microsoft.Phone.Info;
 
 namespace BingSlideshow
 {
@@ -27,12 +28,33 @@ namespace BingSlideshow
         private Stack<String> urls = new Stack<String>();
         private double imageWidth = 0;
         private int imageWidthCounter = 0;
+        
 
         // Constructor
         public MainPage()
         {
             InitializeComponent();
 
+            if (System.Diagnostics.Debugger.IsAttached)
+            {
+                // Display the current frame rate counters.
+                Application.Current.Host.Settings.EnableFrameRateCounter = true;
+            }
+
+            const string total = "DeviceTotalMemory";
+            const string current = "ApplicationCurrentMemoryUsage";
+            const string peak = "ApplicationPeakMemoryUsage";
+
+            long totalBytes =
+              (long)DeviceExtendedProperties.GetValue(total);
+            long currentBytes =
+              (long)DeviceExtendedProperties.GetValue(current);
+            long peakBytes =
+              (long)DeviceExtendedProperties.GetValue(peak);
+
+            textBlock2.Text = totalBytes.ToString();
+            textBlock2.Text = currentBytes.ToString();
+            textBlock2.Text = peakBytes.ToString();
 
             this.wc = new WebClient();
             this.wc.Encoding = System.Text.Encoding.UTF8;
@@ -56,20 +78,41 @@ namespace BingSlideshow
 
         private void Button1_Click(object sender, RoutedEventArgs e)
         {
-            urls = new Stack<String>();
             getResults(TextBox1.Text);
         }
 
         private void getResults(String searchterm){
+            urls.Clear();
             if (!NetworkInterface.GetIsNetworkAvailable()) {
                 MessageBox.Show("Please connect to the internet");
             }
+            int count = 0;
+            System.Diagnostics.Debug.WriteLine("Removing " + (Pan.Items.Count-1) + " items");
+            while (Pan.Items.Count > 1)
+            {
+                //PanoramaItem pi = Pan.Items[1] as PanoramaItem;
+                //Pan.Items.Remove(pi);
+                PanoramaItem pi = Pan.Items[1] as PanoramaItem;
+                Grid grid = pi.Content as Grid;
+                Image img = grid.Children[0] as Image;
+                BitmapImage bitmapImage = img.Source as BitmapImage;
+                bitmapImage.UriSource = null;
+                img.Source = null;
+                GC.Collect();
+                GC.SuppressFinalize(img);
+                GC.WaitForPendingFinalizers();
+                
+                Pan.Items.RemoveAt(1);
+                count++;
+            }
+            System.Diagnostics.Debug.WriteLine("Removed " + count + " items");
             String url = "http://api.bing.net/json.aspx?AppId=A34B1552C3B3DF826089895CCA0D868F0A81EF9D&Query=" + searchterm + "&Sources=Image&Image.Count=20&Image.Filters=Size:Large";
             wc.OpenReadAsync(new Uri(url));
         }
 
         private void loadImage(String url){
             BitmapImage img = new BitmapImage(new Uri(url));
+            
 
             PanoramaItem pi = new PanoramaItem();
             pi.Orientation = System.Windows.Controls.Orientation.Horizontal;
@@ -78,18 +121,27 @@ namespace BingSlideshow
 
             var image = new Image();
             image.ImageOpened += new EventHandler<RoutedEventArgs>(onImageLoaded);
+            image.ImageFailed += new EventHandler<ExceptionRoutedEventArgs>(onImageError);
             image.Source = img;
+
             image.Stretch = Stretch.Uniform;
             grid.Children.Add(image);
 
             pi.Content = grid;
 
-            //Pan.Items.RemoveAt(0);
             Pan.Items.Add(pi);
+            
         }
 
         private void onImageLoaded(Object sender, RoutedEventArgs e) {
-            MessageBox.Show("Loaded");
+            
+        }
+
+        private void onImageError(Object sender, RoutedEventArgs e) {
+            Image img = sender as Image;
+            Grid grid = img.Parent as Grid;
+            PanoramaItem pi = grid.Parent as PanoramaItem;
+            Pan.Items.Remove(pi);
         }
 
         private void downloadImages(Stack<String> urls) {
@@ -97,8 +149,7 @@ namespace BingSlideshow
             {
                 loadImage(s);
             }
-            step();
-            //startStep();
+            System.Diagnostics.Debug.WriteLine("Images downloaded");
         }
 
         private void startStep(){
@@ -112,36 +163,14 @@ namespace BingSlideshow
         }
 
         private void step(){
-            if (myAnimation.IsEnabled) {
-                myAnimation.Stop();
-            }
-            int counter = Pan.SelectedIndex;
             
-            if (Pan.Items.Count > counter + 1)
-            {
-                Pan.DefaultItem = Pan.Items[counter + 1];
-            }
-            else
-            {
-                Pan.DefaultItem = Pan.Items[0];
-            }
-
-            Image child;
             try
             {
-                PanoramaItem item = Pan.DefaultItem as PanoramaItem;
-                Grid grid = item.Content as Grid;
-                child = grid.Children[0] as Image;
+                Pan.DefaultItem = Pan.Items[1];
             }
             catch (Exception) {
                 Pan.DefaultItem = Pan.Items[0];
-                PanoramaItem item = Pan.DefaultItem as PanoramaItem;
-                Grid grid = item.Content as Grid;
-                child = grid.Children[0] as Image;
             }
-            imageWidth = child.ActualWidth;
-
-            //startAnimation();
         }
 
         private void startAnimation()
@@ -205,8 +234,11 @@ namespace BingSlideshow
                     m = m.NextMatch();
                 }
 
+                System.Diagnostics.Debug.WriteLine("URLs matched");
+
                 //urls holds an array of URLs
                 downloadImages(urls);
+                step();
 
             }
             finally
