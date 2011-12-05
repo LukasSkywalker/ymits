ce0618c73e1813b7bfef4e815b829b0a4b943c09
Lukas Diener <lukas.diener@hotmail.com>
2011-12-05 21:01:50 +0100

    Added Downloads section

--- wp7apps/MusicBird/MainPage.xaml.cs
+++ wp7apps/MusicBird/MainPage.xaml.cs
@@ -21,11 +21,20 @@ using System.Text.RegularExpressions;
 using Microsoft.Xna.Framework.Media;
 using Microsoft.Phone.Tasks;
 using System.Net.NetworkInformation;
+using Microsoft.Phone.BackgroundTransfer;
 
 namespace MusicBird
 {
     public partial class MainPage : PhoneApplicationPage
     {
+        //background transfer object container
+        IEnumerable<BackgroundTransferRequest> transferRequests;
+
+        // Booleans for tracking if any transfers are waiting for user action.
+        bool WaitingForExternalPower;
+        bool WaitingForExternalPowerDueToBatterySaverMode;
+        bool WaitingForNonVoiceBlockingNetwork;
+        bool WaitingForWiFi;
 
         // Timer for updating the UI
         DispatcherTimer _timer;
@@ -85,7 +94,7 @@ namespace MusicBird
                 UpdateState(null, null);
             }
 
-            IsolatedStorageExplorer.Explorer.Start("localhost");
+            //DEBUG IsolatedStorageExplorer.Explorer.Start("localhost");
 
             updatePlaylist();
             updateLibrary();
@@ -456,12 +465,74 @@ namespace MusicBird
 
             private void saveTrack(string filename, string uri)
             {  
-                var dl = new WebClient();
+                /*var dl = new WebClient();
                 dl.OpenReadAsync(new Uri(uri), filename);
                 dl.OpenReadCompleted += new OpenReadCompletedEventHandler(dl_downloadCompleted);
-                dl.DownloadProgressChanged += new DownloadProgressChangedEventHandler(dl_DownloadProgressChanged);
+                dl.DownloadProgressChanged += new DownloadProgressChangedEventHandler(dl_DownloadProgressChanged);*/
 
-                //((ApplicationBarIconButton)(ApplicationBar.Buttons[downButton])).IsEnabled = false;
+                // Check to see if the maximum number of requests per app has been exceeded.
+                if (BackgroundTransferService.Requests.Count() >= 5)
+                {
+                    // Note: Instead of showing a message to the user, you could store the
+                    // requested file URI in isolated storage and add it to the queue later.
+                    MessageBox.Show("The maximum number of background file transfer requests for this application has been exceeded. ");
+                    return;
+                }
+
+                // Get the URI of the file to be transferred from the Tag property
+                // of the button that was clicked.
+                string transferFileName = filename;
+                Uri transferUri = new Uri(uri, UriKind.RelativeOrAbsolute);
+
+
+                // Create the new transfer request, passing in the URI of the file to 
+                // be transferred.
+                BackgroundTransferRequest transferRequest = new BackgroundTransferRequest(transferUri);
+
+                // Set the transfer method. GET and POST are supported.
+                transferRequest.Method = "GET";
+
+                // Get the file name from the end of the transfer URI and create a local URI 
+                // in the "transfers" directory in isolated storage.
+
+                using (IsolatedStorageFile isoStore = IsolatedStorageFile.GetUserStoreForApplication())
+                {
+                    if (!isoStore.DirectoryExists("/shared/transfers"))
+                    {
+                        isoStore.CreateDirectory("/shared/transfers");
+                    }
+                }
+
+                Uri downloadUri = new Uri(filename, UriKind.RelativeOrAbsolute);
+                System.Diagnostics.Debug.WriteLine("filename is " + filename + ".mp3");
+                transferRequest.DownloadLocation = new Uri("/shared/transfers/" + filename + ".mp3", UriKind.Relative);
+
+                // Pass custom data with the Tag property. In this example, the friendly name
+                // is passed.
+                transferRequest.Tag = filename + ".mp3";
+
+                transferRequest.TransferPreferences = TransferPreferences.AllowCellular;
+                transferRequest.TransferPreferences = TransferPreferences.AllowBattery;
+                transferRequest.TransferPreferences = TransferPreferences.AllowCellularAndBattery;
+
+                // Add the transfer request using the BackgroundTransferService. Do this in 
+                // a try block in case an exception is thrown.
+                try
+                {
+                    BackgroundTransferService.Add(transferRequest);
+                    transferRequest.TransferStatusChanged += new EventHandler<BackgroundTransferEventArgs>(transfer_TransferStatusChanged);
+                    transferRequest.TransferProgressChanged += new EventHandler<BackgroundTransferEventArgs>(transfer_TransferProgressChanged);
+                    MessageBox.Show("Transfer was added.");
+                    UpdateUI();
+                }
+                catch (InvalidOperationException ex)
+                {
+                    MessageBox.Show("Unable to add background transfer request. " + ex.Message);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Unable to add background transfer request.");
+                }
             }
 
             void dl_DownloadProgressChanged(object sender, DownloadProgressChangedEventArgs e) {
@@ -660,6 +731,187 @@ namespace MusicBird
                 return TrackListElement.ItemContainerGenerator.ContainerFromItem((sender as MenuItem).DataContext) as ListBoxItem;
             }
 
+            private void UpdateRequestsList()
+            {
+                // The Requests property returns new references, so make sure that
+                // you dispose of the old references to avoid memory leaks.
+                if (transferRequests != null)
+                {
+                    foreach (var request in transferRequests)
+                    {
+                        request.Dispose();
+                    }
+                }
+                transferRequests = BackgroundTransferService.Requests;
+            }
+
+            private void UpdateUI()
+            {
+                System.Diagnostics.Debug.WriteLine("Updating UI");
+                // Update the list of transfer requests
+                UpdateRequestsList();
+
+                // If there are 1 or more transfers, hide the "no transfers"
+                // TextBlock. IF there are zero transfers, show the TextBlock.
+                if (transferRequests.Count<BackgroundTransferRequest>() > 0)
+                {
+                    EmptyTextBlock.Visibility = Visibility.Collapsed;
+                }
+                else
+                {
+                    EmptyTextBlock.Visibility = Visibility.Visible;
+                }
+
+                // Update the TransferListBox with the list of transfer requests.
+                TransferListBox.ItemsSource = transferRequests;
+
+            }
+
+            protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+            {
+                // Reset all of the user action Booleans on page load.
+                WaitingForExternalPower = false;
+                WaitingForExternalPowerDueToBatterySaverMode = false;
+                WaitingForNonVoiceBlockingNetwork = false;
+                WaitingForWiFi = false;
+
+                // When the page loads, refresh the list of file transfers.
+                InitialTransferStatusCheck();
+                UpdateUI();
+            }
+
+            private void InitialTransferStatusCheck()
+            {
+                UpdateRequestsList();
+
+                foreach (var transfer in transferRequests)
+                {
+                    transfer.TransferStatusChanged += new EventHandler<BackgroundTransferEventArgs>(transfer_TransferStatusChanged);
+                    transfer.TransferProgressChanged += new EventHandler<BackgroundTransferEventArgs>(transfer_TransferProgressChanged);
+                    ProcessTransfer(transfer);
+                }
+
+                MessageBox.Show("added event handlers");
+
+                if (WaitingForExternalPower)
+                {
+                    MessageBox.Show("You have one or more file transfers waiting for external power. Connect your device to external power to continue transferring.");
+                }
+                if (WaitingForExternalPowerDueToBatterySaverMode)
+                {
+                    MessageBox.Show("You have one or more file transfers waiting for external power. Connect your device to external power or disable Battery Saver Mode to continue transferring.");
+                }
+                if (WaitingForNonVoiceBlockingNetwork)
+                {
+                    MessageBox.Show("You have one or more file transfers waiting for a network that supports simultaneous voice and data.");
+                }
+                if (WaitingForWiFi)
+                {
+                    MessageBox.Show("You have one or more file transfers waiting for a WiFi connection. Connect your device to a WiFi network to continue transferring.");
+                }
+            }
+
+            private void ProcessTransfer(BackgroundTransferRequest transfer)
+            {
+                switch (transfer.TransferStatus)
+                {
+                    case TransferStatus.Completed:
+
+                        // If the status code of a completed transfer is 200 or 206, the
+                        // transfer was successful
+                        if (transfer.StatusCode == 200 || transfer.StatusCode == 206)
+                        {
+                            // Remove the transfer request in order to make room in the 
+                            // queue for more transfers. Transfers are not automatically
+                            // removed by the system.
+                            RemoveTransferRequest(transfer.RequestId);
+
+                            // In this example, the downloaded file is moved into the root
+                            // Isolated Storage directory
+                            using (IsolatedStorageFile isoStore = IsolatedStorageFile.GetUserStoreForApplication())
+                            {
+                                string filename = transfer.Tag;
+                                if (isoStore.FileExists(filename))
+                                {
+                                    isoStore.DeleteFile(filename);
+                                }
+                                isoStore.MoveFile(transfer.DownloadLocation.OriginalString, filename);
+                            }
+                        }
+                        else
+                        {
+                            // This is where you can handle whatever error is indicated by the
+                            // StatusCode and then remove the transfer from the queue. 
+                            RemoveTransferRequest(transfer.RequestId);
+
+                            if (transfer.TransferError != null)
+                            {
+                                // Handle TransferError if one exists.
+                            }
+                        }
+                        break;
+
+
+                    case TransferStatus.WaitingForExternalPower:
+                        WaitingForExternalPower = true;
+                        break;
+
+                    case TransferStatus.WaitingForExternalPowerDueToBatterySaverMode:
+                        WaitingForExternalPowerDueToBatterySaverMode = true;
+                        break;
+
+                    case TransferStatus.WaitingForNonVoiceBlockingNetwork:
+                        WaitingForNonVoiceBlockingNetwork = true;
+                        break;
+
+                    case TransferStatus.WaitingForWiFi:
+                        WaitingForWiFi = true;
+                        break;
+                }
+            }
+
+            void transfer_TransferStatusChanged(object sender, BackgroundTransferEventArgs e)
+            {
+                ProcessTransfer(e.Request);
+                UpdateUI();
+            }
+
+            void transfer_TransferProgressChanged(object sender, BackgroundTransferEventArgs e)
+            {
+                System.Diagnostics.Debug.WriteLine("Downloaded " + e.Request.BytesReceived/e.Request.TotalBytesToReceive);
+                UpdateUI();
+            }
+
+            private void CancelButton_Click(object sender, EventArgs e)
+            {
+                // The ID for each transfer request is bound to the
+                // Tag property of each Remove button.
+                string transferID = ((Button)sender).Tag as string;
+
+                // Delete the transfer request
+                RemoveTransferRequest(transferID);
+
+                // Refresh the list of file transfers
+                UpdateUI();
+            }
+
+            private void RemoveTransferRequest(string transferID)
+            {
+                // Use Find to retrieve the transfer request with the specified ID.
+                BackgroundTransferRequest transferToRemove = BackgroundTransferService.Find(transferID);
+
+                // Try to remove the transfer from the background transfer service.
+                try
+                {
+                    BackgroundTransferService.Remove(transferToRemove);
+                }
+                catch (Exception e)
+                {
+                    // Handle the exception.
+                }
+            }
+
+            
 
     }
 
