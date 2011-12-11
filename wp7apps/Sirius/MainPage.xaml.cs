73bc65c73929ed8674ddab99e92222348dd3c62e
Lukas Diener <lukas.diener@hotmail.com>
2011-12-11 23:56:39 +0100

    Added various method stubs

--- wp7apps/Sirius/MainPage.xaml.cs
+++ wp7apps/Sirius/MainPage.xaml.cs
@@ -44,6 +44,35 @@ namespace Sirius
 
         private void button1_Click( object sender, RoutedEventArgs e )
         {
+            updateToken();
+            getFreeBusyTimes(new DateTime(2011,12,11,00,00,00), new DateTime(2011,12,12,0,0,0));
+        }
+
+        private void getFreeBusyTimes( DateTime start, DateTime end )
+        {
+            String startString = start.ToString("yyyy-MM-ddTHH:mm:ssZ");
+            String endString = end.ToString("yyyy-MM-ddTHH:mm:ssZ");
+
+            var client = new RestClient();
+            client.BaseUrl = "https://www.googleapis.com";
+
+            System.Diagnostics.Debug.WriteLine("Using auth key " + access_token);
+            var request2 = new RestRequest("calendar/v3/freeBusy?access_token=" + access_token, Method.POST);
+            String body = "{\"timeMin\":\""+startString+"\",\"timeMax\":\""+endString+"\",\"items\":[{\"id\":\"junglekiddy@gmail.com\"}]}";
+            request2.AddParameter("application/json", body, ParameterType.RequestBody);
+
+            client.ExecuteAsync(request2, ( response ) =>
+            {
+                string req = response.Content;
+                MessageBox.Show(req);
+                JObject o = JObject.Parse(req);
+                String st = (string)o["calendars"]["junglekiddy@gmail.com"]["busy"][0]["start"];
+                String en = (string)o["calendars"]["junglekiddy@gmail.com"]["busy"][0]["end"];
+                MessageBox.Show(st+" "+en);
+            });
+        }
+
+        private void updateToken() {
             var settings = IsolatedStorageSettings.ApplicationSettings;
             if(settings.Contains("access_token"))
             {
@@ -57,43 +86,32 @@ namespace Sirius
             {
                 MessageBox.Show("Error getting token");
             }
-
-
-            var client = new RestClient();
-            client.BaseUrl = "https://www.googleapis.com";
-
-            System.Diagnostics.Debug.WriteLine("Using auth key " + access_token);
-            var request2 = new RestRequest("calendar/v3/freeBusy?access_token=" + access_token, Method.POST);
-            String body = "{\"timeMin\":\"2011-12-11T00:00:00Z\",\"timeMax\":\"2011-12-12T00:00:00Z\",\"items\":[{\"id\":\"junglekiddy@gmail.com\"}]}";
-            request2.AddParameter("application/json", body, ParameterType.RequestBody);
-
-            client.ExecuteAsync(request2, ( response ) =>
-            {
-                string req = response.Content;
-                MessageBox.Show(req);
-                JObject o = JObject.Parse(req);
-                String kind = (string)o["kind"];
-                MessageBox.Show(kind);
-            });
-
         }
-    }
-    
-    public class FreeBusy {
-        public string timeMin { get; set; }
-        public string timeMax { get; set; }
-        public Id[] items { get; set; }
-        public FreeBusy( string min, string max ) {
-            timeMax = max;
-            timeMin = min;
-            items = new Id[] { new Id("primary") };
+
+        private void getCalendarList() {
+            /*
+             * GET to https://www.googleapis.com/calendar/v3/users/me/calendarList
+             * maxResults=10
+             * minAccessRole=freeBusyReader
+             * showHidden=true
+             */
+        }
+
+        private void getEvents(){
+            /*
+             * GET from https://www.googleapis.com/calendar/v3/calendars/junglekiddy@gmail.com/events
+             * timeMax=2011-12-11T21:47:14.000Z
+             * timeMin=2011-12-11T18:47:14.000Z
+             */
         }
     }
 
-    public class Id{
-        string id { get; set; }
-        public Id(string bla){
-            this.id = bla;
+    public class FreeBusyItem {
+        public String startTime { get; set; }
+        public String endTime { get; set; }
+        public FreeBusyItem(String start, String end) {
+            this.startTime = start;
+            this.endTime = end;
         }
     }
 }
