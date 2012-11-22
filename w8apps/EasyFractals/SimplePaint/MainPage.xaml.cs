0d7fca9d18e607cebe4efd9053a76b7fd164fe08
Lukas Diener <lukas.diener@hotmail.com>
2012-11-22 22:54:08 +0100

    EF version 2

--- w8apps/EasyFractals/SimplePaint/MainPage.xaml.cs
+++ w8apps/EasyFractals/SimplePaint/MainPage.xaml.cs
@@ -30,24 +30,12 @@ namespace SimplePaint
         const double MAX_VALUE_EXTENT = 1.5;
         const double MAX_NORM = MAX_VALUE_EXTENT * MAX_VALUE_EXTENT;
         private Windows.UI.Core.CoreDispatcher dispatcher;
-        private Color myColor;
         private WriteableBitmap bitmap;
-        //DataTransferManager dataTransferManager;
 
         public MainPage()
         {
             this.InitializeComponent();
             this.dispatcher = Window.Current.CoreWindow.Dispatcher;
-            //this.dataTransferManager = DataTransferManager.GetForCurrentView();
-            //dataTransferManager.DataRequested += new TypedEventHandler<DataTransferManager, DataRequestedEventArgs>(this.ShareImageHandler);
-            RSlider.Value = 255;
-            GSlider.Value = 120;
-            BSlider.Value = 0;
-
-            SaveButton.Visibility = Visibility.Collapsed;
-            RedrawButton.Visibility = Visibility.Collapsed;
-            SizeWarning.Visibility = Visibility.Collapsed;
-            CPUWarning.Visibility = Visibility.Collapsed;
         }
 
         /// <summary>
@@ -57,34 +45,19 @@ namespace SimplePaint
         /// property is typically used to configure the page.</param>
         protected override void OnNavigatedTo(NavigationEventArgs e)
         {
-            Smoothness.Items.Clear();
-            for (int i = 5; i < 256; i += 5)
-                Smoothness.Items.Add(i);
-            Smoothness.SelectedIndex = 10;
         }
 
-        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            String val = (string)FractalSelector.SelectedValue;
-            GenerateFractal(val);
-        }
 
         private async void GenerateFractal(String name)
         {
-            bool showInvalidSize = false;
+            int width = (int)FractalPanel.Width;
+            int height = (int)FractalPanel.Height;
 
-            int width = 1024;
-            try{ width = Convert.ToInt32(ImageWidthBox.Text); }
-            catch (FormatException ex) { showInvalidSize = true; }
-            
-            int height = 768;
-            try { height = Convert.ToInt32(ImageHeightBox.Text); }
-            catch (FormatException ex) { showInvalidSize = true; }
+            Random rand = new Random(DateTime.Now.Millisecond);
 
-            if (showInvalidSize)
-            {
-                MessageDialog md = new MessageDialog("Invalid size, using 1024x768 pixels"); await md.ShowAsync();
-            }
+            int red = rand.Next(0, 255);
+            int green = rand.Next(0, 255);
+            int blue = rand.Next(0, 255);
 
             byte[] buffer = new byte[width*height*4];
 
@@ -96,7 +69,6 @@ namespace SimplePaint
                 {
                     double prog = (double)i / (double)height;
                     Progress.Value = prog;
-                    //ProgressText.Text = ((int)(prog * 100)).ToString()+"%";
                 });
                 double y = (height / 2 - i) * scale;
                 for (int j = 0; j < width; j++)
@@ -107,22 +79,20 @@ namespace SimplePaint
                     else if (name == "Julia") color = CalcJuliaSetColor(new ComplexNumber(x, y));
                     else color = 0;
                     int pos = 4 * j + 4 * width * i;
-                    buffer[pos] = (byte)(Convert.ToInt32(myColor.B) * color);      //B
-                    buffer[pos + 1] = (byte)(Convert.ToInt32(myColor.G) * color);  //G
-                    buffer[pos + 2] = (byte)(Convert.ToInt32(myColor.R) * color);  //R
+                    buffer[pos] = (byte)(blue * color);      //B
+                    buffer[pos + 1] = (byte)(green * color);  //G
+                    buffer[pos + 2] = (byte)(red * color);  //R
                     buffer[pos + 3] = 255;                  //A
                 }
             }
             bitmap = new WriteableBitmap(width, height);
             bitmap.FromByteArray(buffer);
             FractalPanel.Source = bitmap;
-            SaveButton.Visibility = Visibility.Visible;
-            RedrawButton.Visibility = Visibility.Visible;
         }
 
         private double CalcMandelbrotSetColor(ComplexNumber c)
         {
-            int MAX_ITERATIONS = (int)Smoothness.SelectedValue;
+            int MAX_ITERATIONS = 150;
             int iteration = 0;
             ComplexNumber z = new ComplexNumber(0, 0);
             do
@@ -137,7 +107,7 @@ namespace SimplePaint
         }
 
         private double CalcJuliaSetColor(ComplexNumber z) {
-            int MAX_ITERATIONS = (int)Smoothness.SelectedValue;
+            int MAX_ITERATIONS = 150;
             int iteration = 0;
             ComplexNumber c = new ComplexNumber(-1.125, 0.25);
             do
@@ -151,76 +121,16 @@ namespace SimplePaint
                 return 0;
         }
 
-        private void Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
         {
-            byte R, G, B, A;
-
-            A = 255;
-            R = Convert.ToByte(RSlider.Value);
-            G = Convert.ToByte(GSlider.Value);
-            B = Convert.ToByte(BSlider.Value);
-
-            myColor = Color.FromArgb(A, R, G, B);
-
-            showColor.Fill = new SolidColorBrush(myColor);
+            GenerateFractal("Mandelbrot");
         }
 
-        private async void SaveButton_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-            String filename = "EasyFractal_" + DateTime.Now.ToString("yyyy-MM-dd_HH.mm.ss")+".png";
-
-            await bitmap.SaveAsync(ImageDirectories.PicturesLibrary, ImageFormat.Png, filename, (uint)bitmap.PixelWidth, (uint)bitmap.PixelHeight);
-
-            MessageDialog md = new MessageDialog("Image was saved to your pictures as " + filename, "Saved");
-            await md.ShowAsync();
+            GenerateFractal("Julia");
         }
-
-        private void RedrawButton_Click_1(object sender, RoutedEventArgs e)
-        {
-            String val = (string)FractalSelector.SelectedValue;
-            GenerateFractal(val);
-        }
-
-        private void SizeBox_KeyUp(object sender, Windows.UI.Xaml.Input.KeyRoutedEventArgs e)
-        {
-            try
-            {
-                if (Convert.ToInt32(ImageHeightBox.Text) > FractalPanel.Height || Convert.ToInt32(ImageWidthBox.Text) > FractalPanel.Width)
-                    SizeWarning.Visibility = Visibility.Visible;
-                else
-                    SizeWarning.Visibility = Visibility.Collapsed;
-                if (Convert.ToInt32(ImageHeightBox.Text) > 3500 || Convert.ToInt32(ImageWidthBox.Text) > 5000)
-                    CPUWarning.Visibility = Visibility.Visible;
-                else
-                    CPUWarning.Visibility = Visibility.Collapsed;
-            }
-            catch (FormatException ex) {
-                /*MessageDialog md = new MessageDialog("Invalid size");
-                md.ShowAsync();*/
-            }
-        }
-
-        /*private async void SaveButton_Click_1(object sender, RoutedEventArgs e)
-        {
-            DataTransferManager dataTransferManager = DataTransferManager.GetForCurrentView();
-
-            
-        }
-
-        private async void ShareImageHandler(DataTransferManager sender, DataRequestedEventArgs e)
-        {
-            DataRequest request = e.Request;
-            request.Data.Properties.Title = "EasyFractals";
-            request.Data.Properties.Description = "A beautiful fractal";
-
-            InMemoryRandomAccessStream  ras = new InMemoryRandomAccessStream();
-            using (Stream stream = await bitmap.PixelBuffer.OpenStreamForReadAsync())
-            {
-                await stream.CopyToAsync(ras.AsStreamForWrite());
-            }
-
-            request.Data.Properties.Thumbnail = bitmap.PixelBuffer.
-        }*/
     }
 
     struct ComplexNumber
